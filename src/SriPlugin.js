12dfbf2661425ba35309835cf5735d95f3041997
Jordan Pittman <jordan@cryptica.me>
2020-10-28 20:28:47 -0400

    Refactor

--- src/SriPlugin.js
+++ src/SriPlugin.js
@@ -6,35 +6,39 @@ class SriPlugin {
   }
 
   apply(compiler) {
-    compiler.plugin('done', stats => {
-      let assets = Object.assign({}, stats.toJson().assetsByChunkName)
-      let hashes = {}
+    compiler.plugin('done', stats => this.writeManifest(stats))
+  }
 
-      flatten(assets).forEach(asset => {
-        if (asset[0] !== '/') {
-          asset = `/${asset}`
-        }
+  writeManifest(stats) {
+    let assets = Object.assign({}, stats.toJson().assetsByChunkName)
+    let hashes = {}
 
-        asset = asset.replace(/\?id=\w{20}/, '')
+    flatten(assets).forEach(asset => {
+      if (asset[0] !== '/') {
+        asset = `/${asset}`
+      }
 
-        let hash = require('crypto')
-          .createHash(this.options.algorithm)
-          .update(
-            require('fs').readFileSync(
-              path.join(require('process').cwd(), Config.publicPath, asset),
-              'utf8'
-            )
-          )
-          .digest('base64')
+      asset = asset.replace(/\?id=\w{20}/, '')
 
-        hashes[asset] = `${this.options.algorithm}-${hash}`
-      })
-
-      require('fs').writeFileSync(
-        path.join(require('process').cwd(), Config.publicPath, 'mix-sri.json'),
-        JSON.stringify(hashes, null, 4)
-      )
+      hashes[asset] = `${this.options.algorithm}-${this.generateHash(asset)}`
     })
+
+    require('fs').writeFileSync(
+      path.join(require('process').cwd(), Config.publicPath, 'mix-sri.json'),
+      JSON.stringify(hashes, null, 4)
+    )
+  }
+
+  generateHash(asset) {
+    return require('crypto')
+      .createHash(this.options.algorithm)
+      .update(
+        require('fs').readFileSync(
+          path.join(require('process').cwd(), Config.publicPath, asset),
+          'utf8'
+        )
+      )
+      .digest('base64')
   }
 }
 
